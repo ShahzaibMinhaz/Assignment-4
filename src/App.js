3e7d530ce166405700e27507f744f4bec95da2fd
Shahzaib Minhaz <shahzaibratancy@gmail.com>
2020-06-25 01:05:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className={` ${isNight ? 'boxlight' : 'box'}`}>
-      <h1>Is Night : {isNight ? 'true':'False'}</h1>
+      <h1>Its Night : {isNight ? 'true':'False'}</h1>
       <Message className="message" counter={count} />
       <br/>
       <button onClick={()=> setCount(++count)}>UPDATE</button>
